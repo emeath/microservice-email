e94fc06ae50f023e056031478505194e5b95a830
emeath <matheus.martins.jeronimo@gmail.com>
2022-07-30 15:53:06 -0300

    Adds email model

---
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,24 @@
+package com.ms.email.models;
+
+import com.ms.email.enums.StatusEmail;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+public class EmailModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String contentBody;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+}
